89672dfdb92cf87ee1af0c478265fd36a2be0eeb
tinyherocarrot <andrw.huang@gmail.com>
2017-03-19 17:02:29 -0700

    move to new GitHub account
    first draft of home page, 2 proj views, rough draft of About Me

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+// BACK TO TOP FUNCTIONALITY
+	$(".back-to-top").click(function() {
+		 	$('html, body').animate({
+		    	scrollTop: "0px"
+		 	}, 400);
+		})
+
+//THUMBNAIL HOVER DARKEN
+})
