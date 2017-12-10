53ae1c4af29a988f18c20430955de15addbd6ec8
Andrew Huang <andrw.huang@gmail.com>
2017-12-09 22:41:18 -0600

    WIP

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,16 +13,20 @@ $(document).ready(function() {
 	function unfixNav() {
     $(".navbar").removeClass("is-fixed-top");
     $("html").removeClass("has-navbar-fixed-top");
-  }
+	}
 
 
 	// main logic
 	$(document).scroll(function() {
-		console.log($(window).scrollTop(), screenheight)
 		if ($(window).scrollTop() > 50) {
 			fixNav();
 		} else {
 			unfixNav();
 		}
 	})
+	$("article").click(function() {
+    $(this).toggleClass("tile");
+    $(this).toggleClass("is-child");
+    $(this).toggleClass("on-display");
+  });
 })
